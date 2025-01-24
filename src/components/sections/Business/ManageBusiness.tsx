9c15eac2aa799714e1238eb67142bd9f9f8b739c
Subx1s0o <nazarsabardin@gmail.com>
2025-01-24 16:02:51 +0200

    feat: add business placeholder

--- src/components/sections/Business/ManageBusiness.tsx
+++ src/components/sections/Business/ManageBusiness.tsx
@@ -1,3 +1,11 @@
 export default function ManageBusiness() {
-    return <div>ManageBusiness</div>;
+    return (
+        <div className="h-[calc(100vh-300px)]">
+            <div className="flex h-full w-full items-center justify-center">
+                <h1 className="text-balance text-center text-md font-semibold">
+                    Very soon you'll can manage your own businesses!
+                </h1>
+            </div>
+        </div>
+    );
 }
